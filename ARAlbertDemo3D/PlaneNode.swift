ad4914832f7b4ceca3b9406f6d18d8cf555339d1
Glenna Buford <glenna.buford@wooga.net>
2017-09-01 21:05:46 -0400

    the demo

---
+++ ARAlbertDemo3D/PlaneNode.swift
@@ -0,0 +1,53 @@
+//
+//  Plane.swift
+//  ARAlbertDemo3D
+//
+//  Created by Glenna L Buford on 8/23/17.
+//  Copyright © 2017 Glenna L Buford. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+class PlaneNode : SCNNode {
+    
+    var plane: SCNPlane!
+    var childPlaneNode: SCNNode!
+    
+    public init(withAnchor anchor: ARPlaneAnchor) {
+        super.init()
+        
+        self.plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        let floorMaterial = SCNMaterial()
+        floorMaterial.diffuse.contents = #imageLiteral(resourceName: "hardwood")
+        floorMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(anchor.extent.x, anchor.extent.z, 1)
+        floorMaterial.diffuse.wrapT = .repeat
+        floorMaterial.diffuse.wrapS = .repeat
+        self.plane.materials = [floorMaterial]
+        
+        self.childPlaneNode = SCNNode(geometry: self.plane)
+        self.childPlaneNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+        self.childPlaneNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        
+        self.addChildNode(self.childPlaneNode)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    public func update(withAnchor anchor: ARPlaneAnchor) {
+        self.plane.width = CGFloat(anchor.extent.x)
+        self.plane.height = CGFloat(anchor.extent.z)
+        if let floorMaterial = self.plane.firstMaterial {
+            floorMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(anchor.extent.x,anchor.extent.z, 1)
+            floorMaterial.diffuse.wrapT = .repeat
+            floorMaterial.diffuse.wrapS = .repeat
+        }
+        
+        let physics = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: self.plane, options: nil))
+        self.childPlaneNode.physicsBody = physics
+        self.childPlaneNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+    }
+}
---
+++ ARAlbertDemo3DTests/ARAlbertDemo3DTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARAlbertDemo3DTests.swift
+//  ARAlbertDemo3DTests
+//
+//  Created by Glenna L Buford on 7/2/17.
+//  Copyright © 2017 Glenna L Buford. All rights reserved.
+//
+
+import XCTest
+@testable import ARAlbertDemo3D
+
+class ARAlbertDemo3DTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
