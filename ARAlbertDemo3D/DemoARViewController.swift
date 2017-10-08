f312e43e84af4e4f04a97002d8757cd55343d55c
Glenna Buford <glenna.buford@wooga.net>
2017-10-08 21:33:38 +0200

    Initial version of face detection and face landmarks detect

---
+++ ARAlbertDemo3D/DemoARViewController.swift
@@ -0,0 +1,151 @@
+//
+//  ViewController.swift
+//  ARAlbertDemo3D
+//
+//  Created by Glenna L Buford on 7/2/17.
+//  Copyright © 2017 Glenna L Buford. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class DemoARViewController: UIViewController, ARSCNViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        self.sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        self.sceneView.showsStatistics = true
+                
+        //show the feature points
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        self.sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        self.sceneView.session.pause()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return nil }
+        return PlaneNode(withAnchor: planeAnchor)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard
+            let planeAnchor = anchor as? ARPlaneAnchor,
+            let planeNode = node as? PlaneNode
+        else { return }
+
+        planeNode.update(withAnchor: planeAnchor)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        let tapPoint = sender.location(in: sceneView)
+        let results = sceneView.hitTest(tapPoint, types: ARHitTestResult.ResultType.existingPlaneUsingExtent)
+        
+        if let arhitresult = results.first {
+            addABox(at: arhitresult)
+        }
+    }
+    
+    // MARK: more ARSCNViewDelegate
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        print("session didFailWithError \(session)")
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        print("session interrupted \(session)")
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        print("session ended \(session)")
+    }
+    
+    func addABox(at hitPoint: ARHitTestResult) {
+        let cubeNode = BoxyNode()
+        cubeNode.position = positionFromHitTestResult(hitPoint)
+
+        self.sceneView.scene.rootNode.addChildNode(cubeNode)
+        
+        let anchor = ARAnchor(transform: hitPoint.worldTransform)
+        self.sceneView.session.add(anchor: anchor)
+        self.boxes.append(cubeNode)
+    }
+    
+    func positionFromHitTestResult(_ hitPoint: ARHitTestResult) -> SCNVector3 {
+        let yOffset: Float = 0.5
+        return SCNVector3Make(hitPoint.worldTransform.columns.3.x,
+                              hitPoint.worldTransform.columns.3.y + yOffset,
+                              hitPoint.worldTransform.columns.3.z)
+    }
+    
+    
+    // MARK: - More Actions
+    
+    @IBAction func onRefreshPressed(_ sender: UIButton) {
+        let sessionConfig = ARWorldTrackingConfiguration()
+        if planeDetectionSwitch.isOn {
+            sessionConfig.planeDetection = .horizontal
+        }
+        self.sceneView.session.run(sessionConfig, options: [.resetTracking, .removeExistingAnchors])
+        self.boxes.removeAll()
+    }
+    
+    @IBAction func onPlaneDetectionSwitchChanged(_ sender: UISwitch) {
+        //stop/start plane detection
+        let sessionConfig = ARWorldTrackingConfiguration()
+        if sender.isOn {
+            sessionConfig.planeDetection = .horizontal
+        }
+        
+        self.sceneView.session.run(sessionConfig, options: [])
+    }
+    
+    @IBAction func onFeaturePointSwitchChanged(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        } else {
+            self.sceneView.debugOptions = []
+        }
+    }
+    
+    @IBOutlet weak var planeDetectionSwitch: UISwitch!
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var featurePointSwitch: UISwitch!
+    var boxes = Array<SCNNode>() {
+        didSet {
+            print("number of boxes \(boxes.count)")
+        }
+    }
+}
---
+++ ARAlbertDemo3D/DemoVNViewController.swift
@@ -0,0 +1,119 @@
+//
+//  DemoVNViewController.swift
+//  ARAlbertDemo3D
+//
+//  Created by Glenna L Buford on 9/26/17.
+//  Copyright © 2017 Glenna L Buford. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class DemoVNViewController: UIViewController {
+    
+    @IBOutlet weak var glennaImageView: UIImageView!
+    @IBOutlet weak var bezierPathView: UIView!
+    
+    let faceDetection = VNDetectFaceRectanglesRequest()
+    let faceDetectionRequest = VNSequenceRequestHandler()
+    
+    let faceLandmarksDetectionRequest = VNSequenceRequestHandler()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //because vision framework operates in the opposite coordinate system
+        bezierPathView.layer.sublayerTransform = CATransform3DMakeScale(1.0, -1.0, 1.0);
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard let image = glennaImageView.image else {
+            fatalError("wtf")
+        }
+        
+        let faceLandmarksRequest = VNDetectFaceLandmarksRequest { (request, error) in
+            if let results = request.results as? [VNFaceObservation] {
+                print(results)
+                for faceObservation in results {
+                    
+                    guard let faceLandmarks = faceObservation.landmarks else {
+                        continue
+                    }
+                    
+                    if let nose = faceLandmarks.nose {
+                        let nosePoints = nose.pointsInImage(imageSize: self.glennaImageView.bounds.size)
+                        
+                        self.drawPointsOnImage(nosePoints)
+                    }
+                    
+                    if let rightEye = faceLandmarks.rightEye {
+                        let rightEyePoints = rightEye.pointsInImage(imageSize: self.glennaImageView.bounds.size)
+                        
+                        self.drawPointsOnImage(rightEyePoints)
+                    }
+                    
+                    if let leftEye = faceLandmarks.leftEye {
+                        let leftEyePoints = leftEye.pointsInImage(imageSize: self.glennaImageView.bounds.size)
+                        
+                        self.drawPointsOnImage(leftEyePoints)
+                    }
+                    
+                    if let rightEyebrow = faceLandmarks.rightEyebrow {
+                        let rightEyebrowPoints = rightEyebrow.pointsInImage(imageSize: self.glennaImageView.bounds.size)
+                        
+                        self.drawPointsOnImage(rightEyebrowPoints)
+                    }
+                    
+                    if let leftEyebrow = faceLandmarks.leftEyebrow {
+                        let leftEyebrowPoints = leftEyebrow.pointsInImage(imageSize: self.glennaImageView.bounds.size)
+                        
+                        self.drawPointsOnImage(leftEyebrowPoints)
+                    }
+                    
+                    if let outerLips = faceLandmarks.outerLips {
+                        let outerLipsPoints = outerLips.pointsInImage(imageSize: self.glennaImageView.bounds.size)
+                        
+                        self.drawPointsOnImage(outerLipsPoints)
+                    }
+                }
+            }
+        }
+        
+        // vision
+        let requestHandler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+        
+        try? requestHandler.perform([faceLandmarksRequest])
+    }
+    
+    func drawPointsOnImage(_ points: [CGPoint]) {
+        
+        let bezierPath = UIBezierPath.pathWithPoints(points)
+        
+        let bezierPathLayer = CAShapeLayer()
+        bezierPathLayer.strokeColor = UIColor.red.cgColor
+        bezierPathLayer.fillColor = UIColor.red.cgColor
+        bezierPathLayer.lineWidth = 5.0
+        
+        bezierPathLayer.path = bezierPath.cgPath
+        
+        self.bezierPathView.layer.addSublayer(bezierPathLayer)
+    }
+}
+
+extension UIBezierPath {
+    class func pathWithPoints(_ points: [CGPoint]) -> UIBezierPath {
+        let path = UIBezierPath()
+        path.lineWidth = 2.0
+        
+        path.move(to: points[0])
+        let otherPoints = points.suffix(points.count - 1)
+        
+        for point in otherPoints {
+            path.addLine(to: point)
+        }
+        
+        path.close()
+        
+        return path
+    }
+}
